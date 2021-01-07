775489966a44ee0baa22f42715239b0c179a9ecb
Aiman Saad <aimansaad84@gmail.com>
2021-01-07 18:22:55 +0500

    finalized task 2

--- task 2/task 2.js
+++ task 2/task 2.js
@@ -1,9 +1,11 @@
 var names = [];
 var scores = [];
+var percnt = [];
 for (var i = 0; i < 3; i++ ) {
     names[i] = prompt("enter your name");
     scores[i] = prompt("enter you score");
+    percnt[i] = scores[i] * .2;
 } 
 for (var j = 0; j < 3; j++ ) {
-document.write("<br>Score of " + names[j] + "is " + scores[j]);
+document.write("<br> <h2>Score of " + names[j] + " is " + scores[j] + ". Percentage is: " + percnt[j] + "%");
 }
