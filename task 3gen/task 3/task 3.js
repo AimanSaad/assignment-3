bc2d6984f846fb71ae2799413000443db1d56aca
Aiman Saad <aimansaad84@gmail.com>
2021-01-07 20:16:07 +0500

    finalized task 3a

---
+++ task 3gen/task 3/task 3.js
@@ -0,0 +1,4 @@
+var cN = ["yellow", "green"];
+for(var i = 0; i < 3;i++) {
+    document.write("<br> " + cN[i]);
+    }
---
+++ task 3gen/task 3a/task 3a.js
@@ -0,0 +1,6 @@
+var cN = ["yellow", "green"];
+cN.unshift(prompt("which color do you want to add in the begining?"));
+for(var i = 0; i < 3;i++) {
+    document.write("<br> " + cN[i]);
+    }
+
