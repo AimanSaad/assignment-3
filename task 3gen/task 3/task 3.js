25a75edbf6dffa49b15698ff927eb117ed4e71b6
Aiman Saad <aimansaad84@gmail.com>
2021-01-07 22:33:36 +0500

    updated task 3.js and added 3b

--- task 3gen/task 3/task 3.js
+++ task 3gen/task 3/task 3.js
@@ -1,4 +1,4 @@
 var cN = ["yellow", "green"];
-for(var i = 0; i < 3;i++) {
+for(var i = 0; i < 2;i++) {
     document.write("<br> " + cN[i]);
     }
---
+++ task 3gen/task 3b/task 3b.js
@@ -0,0 +1,5 @@
+var cN = ["yellow", "green"];
+cN.push(prompt("which color do you want to add at the end?"));
+for(var i = 0; i < 3;i++) {
+    document.write("<br> " + cN[i]);
+    }
