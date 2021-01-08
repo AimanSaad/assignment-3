f3f9ebe67369317db5a311be5abd02ec3db12eb7
Aiman Saad <aimansaad84@gmail.com>
2021-01-08 19:40:25 +0500

    finalized task  3f

---
+++ task 3gen/task 3f/task 3f.js
@@ -0,0 +1,7 @@
+var cN = ["green", "pink", "red", "purple", "blue", "brown", "white"]
+var cN1 = parseInt(prompt("at which index you want to add a new color?"));
+var cN2 = prompt("which color you want to add");
+cN.splice(cN1, 0, cN2);
+for (var i = 0; i < 8; i++) {
+document.write("<br>" + cN[i]);
+}
