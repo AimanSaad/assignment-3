f4669a445d5a5a93e23c8ae2e3cb486d8808a5b8
Aiman Saad <aimansaad84@gmail.com>
2021-01-12 18:45:28 +0500

    finalized task 11

--- task 11/task 11.js
+++ task 11/task 11.js
@@ -1,10 +1,12 @@
 var A = ["cake", "apple pie", "cookie", "chips", "patties"];
 var search = prompt("Welcome to ABC Bakery. What do you want to order sir/ ma'am?");
+var found;
 for (var i = 0; i < A.length; i++){   
 if (search === A[i]) {
+    found = true;
     document.write(A[i] + " is available at index " + i +" in our bakery");
-}
-else {
-     document.write("a");
+     break;
  }
 }
+found = false;
+document.write("Sorry! " + search + "  is NOT available at our bakery");
--- task 8/task 8e/task 8e.js
+++ task 8/task 8e/task 8e.js
@@ -1,6 +1,6 @@
 var ser = [];
 for (var i = 2; i < 21; i+2) {
-    ser[i] = i * 1000;
-    document.write(ser[i]);
+    ser[i] = i ;
+    document.write(ser[i] + "K ");
 
 }
